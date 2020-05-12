7dd5bdbd845c3d2d49f8a629e5a56445102aadb2
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-05-12 20:35:44 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  configure do
+    enable :sessions
+    set :session_secret, "487329585352"
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  get '/checkout' do
+    session[:item] = params[:item]
+    @session = session
+
+  end
 end
